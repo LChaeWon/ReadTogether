8cb7ec0a1d7cf6fc5407d3bc6b14eb748aa55d28
LChaeWon <dcodnjs12@naver.com>
2023-10-22 19:08:40 +0900

    design: header.html 구성

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
+from accountapp.views import main_page
 
 app_name = "accountapp"
 
 urlpatterns = [
-
+    path('main/', main_page, name='main')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def main_page(request):
+    return render(request, 'accountapp/main_page.html')
