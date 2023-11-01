cf4fae22706db74e951c9c3f39154460f0d7ec8c
LChaeWon <dcodnjs12@naver.com>
2023-11-01 19:27:50 +0900

    feat: 마이페이지 기능 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 
 def main_page(request):
@@ -15,3 +15,9 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:main')
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
