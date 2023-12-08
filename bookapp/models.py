cabbbc480eb622317af6a6177e109b80a57a36e0
LChaeWon <dcodnjs12@naver.com>
2023-12-08 19:24:49 +0900

    feat: 책 키워드 선택 추가

--- bookapp/models.py
+++ bookapp/models.py
@@ -1,11 +1,17 @@
 
-
 from django.db import models
 
+
+KEYWORD_CHOICES=[
+    ('novel','소설'),
+    ('human','인문'),
+    ('self','자기계발'),
+    ('science','과학')
+]
 class Book(models.Model):
     title = models.CharField(max_length=200)
     writer = models.CharField(max_length=200)
     image = models.ImageField(upload_to='book/')
-    keyword = models.CharField(max_length=5)
+    keyword = models.CharField(max_length=10,choices=KEYWORD_CHOICES)
 
 
